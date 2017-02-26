bbdaf57db886d9c94ff67623151de65505d75f2b
Seth Rader <seth@Seths-MacBook-Air.local>
2017-02-25 22:26:28 -0500

    Added structures, functions - Map_brush and Map_texData structures - Functions to create and destroy entities

--- map.h
+++ map.h
@@ -3,14 +3,6 @@ struct Map_ivec3
     int xyz[3];
 };
 
-struct Map_entity
-{
-    struct kvPair *kvList;
-    unsigned int pairCount;
-    struct face *faces;
-    unsigned int faceCount;
-};
-
 struct Map_kvPair
 {
     char *key;
@@ -19,13 +11,7 @@ struct Map_kvPair
     unsigned int valueSz;
 };
 
-struct Map_face
-{
-    struct Map_ivec3 plane[3];
-    struct Map_texData;
-};
-
-struct texData
+struct Map_texData
 {
     float scale[2];
     float offset[2];
@@ -33,3 +19,27 @@ struct texData
     char *texName;
     unsigned int texNameSz;
 };
+
+struct Map_face
+{
+    struct Map_ivec3 plane[3];
+    struct Map_texData;
+};
+
+struct Map_brush
+{
+    struct Map_face *faces;
+    unsigned int faceCount;
+}
+
+struct Map_entity
+{
+    struct Map_kvPair *kvList;
+    unsigned int pairCount;
+    struct Map_brush *brushes;
+    unsigned int brushCount;
+};
+
+struct Map_entity *Map_newEntityFromFile(const char *fileName);
+
+void Map_freeEntity(struct Map_entity *entity);
