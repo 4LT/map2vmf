fae754ce0be021b5c1a353b44b897308ea76a45c
seth <rader.seth@gmail.com>
2017-02-26 16:01:40 -0500

    Added guards and included stdlib

--- util/reszarr.h
+++ util/reszarr.h
@@ -1,3 +1,8 @@
+#ifndef RESZARR_H_
+#define RESZARR_H_
+
+#include <stdlib.h>
+
 struct ReszArr_Array;
 
 struct ReszArr_Array *ReszArr_create(unsigned int typeSize);
@@ -9,3 +14,5 @@ size_t ReszArr_getCount(struct ReszArr_Array *arr);
 void ReszArr_copy(struct ReszArr_Array *arr, void *dest);
 
 void ReszArr_destroy(struct ReszArr_Array *arr);
+
+#endif
