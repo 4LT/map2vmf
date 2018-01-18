0240d75ef2e7c7c131f81d9500b2b7af39b10b18
seth <rader.seth@gmail.com>
2018-01-17 22:03:07 -0500

    Changed ReszArr_append's declaration's return type to void

--- util/reszarr.h
+++ util/reszarr.h
@@ -7,7 +7,7 @@ struct ReszArr_Array;
 
 struct ReszArr_Array *ReszArr_create(unsigned int typeSize);
 
-struct ReszArr_Array *ReszArr_append(struct ReszArr_Array *arr, void *datum);
+void ReszArr_append(struct ReszArr_Array *arr, void *datum);
 
 size_t ReszArr_getCount(struct ReszArr_Array *arr);
 
